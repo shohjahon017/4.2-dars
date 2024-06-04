a52819ac4be76bad8269c23bfc3f02c256f9fe7c
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-04 08:29:35 +0500

    7-masalani bajardim

---
+++ index.js
@@ -0,0 +1,84 @@
+// let num = +prompt("3 xonali son kiriting");
+// let birlar = num % 10;
+// let onlar = Math.trunc(num / 100);
+// let res = birlar % 10;
+// console.log(birlar, onlar, res);
+
+// let num = +prompt("2 xonali son kiriting");
+// let birlar = num % 10;
+// let onlar = Math.trunc(num / 10);
+// console.log(birlar + onlar);
+
+// let num = +prompt("ikki xonali son kiriting");
+// let birlar = num % 10;
+// let onlar = Math.trunc(num / 10);
+// let res = birlar + "" + onlar;
+// console.log(res);
+
+// let num = +prompt("3 xonali son kiriting");
+// let yuzlar = Math.trunc(num / 10);
+// let res = yuzlar % 10;
+// console.log(res);
+
+// let num = +prompt("3 xonali son kiriting");
+// let yuzlar = Math.trunc(num / 100);
+// let o = yuzlar % 10;
+// let b = Math.trunc(num % 10);
+// let res = (b+''+o +''yuzlar)
+// console.log(res);
+
+//1-masala
+// let num = +prompt("3 xonali son kiriting");
+// let yuzlar = Math.trunc(num / 100);
+// console.log(yuzlar);
+
+//2-masala
+// let num = +prompt("3 xonali son kiriting");
+// let yuzlar = Math.trunc(num / 100);
+// let b = Math.trunc(num % 10);
+// let o = yuzlar % 10;
+// console.log(b, o);
+
+// 3-masala
+
+// let num = +prompt("3 xonali son kiriting");
+// let y = Math.trunc(num / 100);
+// let o = yuzlar % 10;
+// let b = Math.trunc(num % 10);
+// let res = b + "" + o + "" + y;
+// console.log(res);
+
+// 4-masala
+// let a = +prompt("Uch xonali son kiriting");
+// let son = a % 100;
+// let birlik = son % 10;
+// let onlik = son / 10;
+// let yuzlik = a / 100;
+// let i = birlik * 100 + onlik * 10 + yuzlik;
+// console.log(i);
+
+//5-masala
+// let a = +prompt("Uch xonali son kiriting");
+// let son = a % 100;
+// let birlik = son % 10;
+// let onlik = son / 10;
+// let yuzlik = a / 100;
+// let i = onlik * 100 + yuzlik * 10 +  birlik;
+// console.log(i);
+
+// 6-masala
+// let num = +prompt("Uch xonali son kiriting");
+// let onlik = num / 10;
+// let yuzlik = Math.trunc(num / 100);
+// let birlik = num % 10;
+// let i = onlik + "" + yuzlik + "" + birlik;
+
+// 7-masala
+// let num = +prompt("Uch xonali son kiriting");
+// let onlik = num / 10;
+// let yuzlik = Math.trunc(num / 100);
+// let birlik = num % 10;
+// let i =yuzlik + "" +birlik   + "" + onlik;
+
+// 8-masala
+let num = +prompt("To`rt xonali son kiriting");
